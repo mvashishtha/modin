35993dbd761480a3ff34d7263a4fefdac951b213
mvashishtha <mahesh@ponder.io>
2021-10-25 19:02:29 -0700

    Fix the append() race condition.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

---
+++ simple_actors.py
@@ -0,0 +1,70 @@
+from re import split
+from modin.config import NPartitions
+from modin.distributed.dataframe.pandas import partitions, unwrap_partitions
+from modin.distributed.dataframe.pandas import from_partitions
+import numpy as np
+import pandas
+import ray
+import modin.pandas as pd
+import time
+import asyncio
+
+ray.init(num_cpus=8)
+num_partitions = 8
+num_actors = 8
+NPartitions.put(num_partitions)
+
+
+@ray.remote
+class ShuffleActor(object):
+    def __init__(self, pos):
+        self.other_actors = None
+        self.num_position = pos
+        self.list_of_dfs = []
+
+    def add_pool(self, other_actors):
+        self.other_actors = other_actors
+        self.splits = list(range(len(other_actors)))
+
+    def append_df(self, df):
+        self.list_of_dfs.append(df)
+        # print(f"finished appending value {df} to actor {self.num_position}")
+
+    def append_splits(self):
+        # self.append_df(self.num_position)
+        # return [
+        #     actor.append_df.remote(self.num_position)
+        #     for i, actor in enumerate(self.other_actors)
+        #     if i != self.num_position
+        # ]
+        self.append_df(self.num_position)
+        return ray.get(
+            [
+                actor.append_df.remote(self.num_position)
+                for i, actor in enumerate(self.other_actors)
+                if i != self.num_position
+            ]
+        )
+
+    def print_list_of_dfs(self):
+        print(
+            f"splits for actor {self.num_position}: {', '.join(str(x) for x in self.list_of_dfs)}"
+        )
+
+
+start = time.time()
+shuffle_actors = [ShuffleActor.remote(i) for i in range(num_actors)]
+ray.get([actor.add_pool.remote(shuffle_actors) for actor in shuffle_actors])
+print(f"time to add pools: {time.time() - start}")
+# ray.get(
+#     [
+#         ref
+#         for sublist in ray.get(
+#             [actor.append_splits.remote() for actor in shuffle_actors]
+#         )
+#         for ref in sublist
+#     ]
+# )
+ray.get([actor.append_splits.remote() for actor in shuffle_actors])
+print(f"time to append splits: {time.time() - start}")
+ray.get([actor.print_list_of_dfs.remote() for actor in shuffle_actors])
--- sort_non_modin.py
+++ sort_non_modin.py
@@ -1,3 +1,5 @@
+import asyncio
+from re import split
 from modin.config import NPartitions
 from modin.distributed.dataframe.pandas import partitions, unwrap_partitions
 from modin.distributed.dataframe.pandas import from_partitions
@@ -7,13 +9,13 @@ import ray
 import modin.pandas as pd
 import time
 
-ray.init(num_cpus=16)
+ray.init(num_cpus=8)
 num_partitions = 8
 num_actors = 8
 NPartitions.put(num_partitions)
 
 
-@ray.remote(num_cpus=0.001)
+@ray.remote
 class ShuffleActor(object):
     def __init__(self, pos):
         self.list_of_dfs = []
@@ -30,23 +32,27 @@ class ShuffleActor(object):
     def append_df(self, df):
         self.list_of_dfs.append(df)
 
-    def split_df(self, df, split_func):
+    def split_df(self, df, split_func, columns, quants, partition_index):
         print(
             f"Actor {self.num_position} split_df: Starting at time: {time.time() - start}"
         )
-        [
+        splits = split_func(df, self.num_position, columns, quants, partition_index)
+        self.append_df(splits[self.num_position])
+        refs = [
             self.other_actors[i].append_df.remote(df_split)
+            for i, df_split in enumerate(splits)
             if i != self.num_position
-            else self.append_df(df_split)
-            for i, df_split in enumerate(split_func(df, self.num_position))
         ]
         print(
             f"Actor {self.num_position} split_df: Finished at time: {time.time() - start}"
         )
+        return refs
 
 
-def split_func(df, actor_position):
-    print(f"Actor {actor_position} split_func: Starting at time: {time.time() - start}")
+def split_func(df, actor_position, columns, quants, partition_index):
+    print(
+        f"Actor {actor_position} split_func on partition {partition_index}: Starting at time: {time.time() - start}"
+    )
     df = df.sort_values(columns)
     t = np.digitize(df[columns].squeeze(), quants, right=True)
     grouper = df.groupby(t)
@@ -60,52 +66,70 @@ def split_func(df, actor_position):
     return splits
 
 
-df = pd.read_csv("test_1mx256.csv")
 start = time.time()
 
-shuffle_actors = [ShuffleActor.remote(i) for i in range(num_actors)]
-parts = unwrap_partitions(df, axis=0)
-assert (
-    len(parts) == num_partitions
-), f"Dataframe was partitioned into {len(parts)} partitions instead of {num_partitions} partitions."
-ray.get([actor.add_pool.remote(shuffle_actors) for actor in shuffle_actors])
-print(
-    f"Set up {len(shuffle_actors)} actors and {len(parts)} partitions at: {time.time() - start}"
-)
 
-columns = "col2"
-# last quantile is 99.0 but digitize uses <=: https://numpy.org/doc/stable/reference/generated/numpy.digitize.html
-# Solution: right bin is max and use right=True so last bin is <= max and first bin is <= 1/N quantile
-# so every value is captured.
-# Calculating qunatiles is really slow!
-sample = df.iloc[
-    np.sort(np.random.choice(len(df), size=10 * num_partitions, replace=False))
-]._to_pandas()
-quants = [
-    np.quantile(sample[columns], i / num_actors) for i in range(1, num_actors + 1)
-]
-quants[-1] = float(
-    "inf"
-)  # Because of sampling, the max quantile may not be the actual max
-print(f"Got quantiles at: {time.time() - start}")
+def sorted_dataframe():
+    df = pd.read_csv("test_1mx256.csv")
 
-ray.get(
-    [
-        shuffle_actors[i % num_actors].split_df.remote(partition, split_func)
-        for i, partition in enumerate(parts)
-    ]
-)
-print(f"Split dataframes at: {time.time() - start}")
-
-new_parts = [
-    actor.combine_and_apply_dfs.remote(
-        lambda x: x.sort_values(columns),
+    shuffle_actors = [ShuffleActor.remote(i) for i in range(num_actors)]
+    parts = unwrap_partitions(df, axis=0)
+    assert (
+        len(parts) == num_partitions
+    ), f"Dataframe was partitioned into {len(parts)} partitions instead of {num_partitions} partitions."
+    ray.get([actor.add_pool.remote(shuffle_actors) for actor in shuffle_actors])
+    print(
+        f"Set up {len(shuffle_actors)} actors and {len(parts)} partitions at: {time.time() - start}"
     )
-    for actor in shuffle_actors
-]
-print(f"Combined dataframes and sorted each partition at: {time.time() - start}")
 
-df = from_partitions(new_parts, axis=0)
-print(
-    f"Built dataframe from {len(new_parts)} parts and finished at: {time.time() - start}"
-)
+    columns = "col2"
+    # last quantile is 99.0 but digitize uses <=: https://numpy.org/doc/stable/reference/generated/numpy.digitize.html
+    # Solution: right bin is max and use right=True so last bin is <= max and first bin is <= 1/N quantile
+    # so every value is captured.
+    # Calculating qunatiles is really slow!
+    # sample = df.iloc[
+    #     np.sort(np.random.choice(len(df), size=10 * num_partitions, replace=False))
+    # ]._to_pandas()
+    # quants = [
+    #     np.quantile(sample[columns], i / num_actors) for i in range(1, num_actors + 1)
+    # ]
+    quants = [100 * x / num_actors for x in range(1, num_actors + 1)]
+    quants[-1] = float(
+        "inf"
+    )  # Because of sampling, the max quantile may not be the actual max
+    print(f"Got quantiles at: {time.time() - start}")
+
+    ray.get(
+        [
+            ref
+            for sublist in ray.get(
+                [
+                    shuffle_actors[i % num_actors].split_df.remote(
+                        partition, split_func, columns, quants, i
+                    )
+                    for i, partition in enumerate(parts)
+                ]
+            )
+            for ref in sublist
+        ]
+    )
+    print(f"split dataframes at: {time.time() - start}")
+
+    new_parts = [
+        actor.combine_and_apply_dfs.remote(
+            lambda x: x.sort_values(columns),
+        )
+        for actor in shuffle_actors
+    ]
+    ray.get(new_parts)
+    print(f"Combined dataframes and sorted each partition at: {time.time() - start}")
+
+    df = from_partitions(new_parts, axis=0)
+    print(
+        f"Built dataframe from {len(new_parts)} parts and finished at: {time.time() - start}"
+    )
+
+    return df
+
+
+df = sorted_dataframe()
