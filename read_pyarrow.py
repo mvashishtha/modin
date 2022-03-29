4a90a17995bd3230f7297eafeac317428f1a6fdf
mvashishtha <mahesh@ponder.io>
2022-03-29 11:49:15 -0500

    Read chunks of columns only for better performance.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- read_pyarrow.py
+++ read_pyarrow.py
@@ -9,43 +9,31 @@ import ray
 
 
 @ray.remote
-def read_block(
-    path: str, column_start: int, column_end: int, row_start: int, row_end: int
-):
+def read_block(path: str, column_start: int, column_end: int):
     with pa.memory_map(path, "r") as source:
         table = pa.ipc.RecordBatchFileReader(source).read_all()
-    pandas_object = (
-        table.select(list(range(column_start, column_end)))
-        .slice(row_start, row_end - row_start)
-        .to_pandas()
-    )
+    pandas_object = table.select(list(range(column_start, column_end))).to_pandas()
     if isinstance(pandas_object, pandas.Series):
         pandas_object = pandas_object.to_frame()
     return pandas_object
 
 
-# This reads in 10.4 sec on my mac
+# This reads in 5.02 seconds on my mac
 def read_pyarrow(path: str):
     with pa.memory_map(path, "r") as source:
         table = pa.ipc.RecordBatchFileReader(source).read_all()
-    num_rows, num_columns = table.shape
+    num_columns = table.shape[1]
     col_step = compute_chunksize(num_columns, NPartitions.get())
-    row_step = compute_chunksize(num_rows, NPartitions.get())
     return from_partitions(
         [
-            [
-                read_block.remote(
-                    path,
-                    col_start,
-                    col_start + col_step,
-                    row_start,
-                    row_start + row_step,
-                )
-                for col_start in range(0, num_columns, col_step)
-            ]
-            for row_start in range(0, num_rows, row_step)
+            read_block.remote(
+                path,
+                col_start,
+                col_start + col_step,
+            )
+            for col_start in range(0, num_columns, col_step)
         ],
-        axis=None,
+        axis=1,
         index=table.select([]).to_pandas().index,
     )
 
